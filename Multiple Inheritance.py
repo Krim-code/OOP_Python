5611bf3e8e8f144df817c5839828e04cdad1f560
Krim-code <suhetsky.gricha555@gmail.com>
2023-06-03 20:51:13 +0300

    WTF???

--- Multiple Inheritance.py
+++ Multiple Inheritance.py
@@ -2,15 +2,28 @@ class Car:
     def drive(self):
         print("I can drive")
 
+    def fly(self):
+        print("I can`t fly")
+
 
 class Bird:
+    def drive(self):
+        print("I can`t drive")
+
     def fly(self):
         print("I can fly")
 
 
-class Jet(Car, Bird):
-    pass
+class Test:
+    def test(self):
+        print("ttttteeeeeest")
+
+
+class Jet(Car, Bird, Test):
+    def fly(self):
+        super(Car,self).fly()
 
 
 Jet().drive()
 Jet().fly()
+Jet().test()
